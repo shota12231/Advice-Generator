29d40ec9b6898d7bab4dcbc625f50351652709bd
shota12231 <167452117+shota12231@users.noreply.github.com>
2025-06-02 22:24:39 +0400

    done

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// getting elements
 const diceBtn = document.getElementById("diceIcon");
 const adviceNum = document.getElementById("adviceNum");
 const adviceDemo = document.getElementById("adviceDemo");
-let count = 1;
-
+// buttons function
 diceBtn.addEventListener("click", () => {
-  count++;
-  adviceDemo.innerHTML = "random quote from api";
-  adviceNum.innerHTML = count;
+  adviceDemo.innerHTML = "Loading...";
+  fetch("https://api.adviceslip.com/advice", {
+    method: "GET",
+    cache: "no-cache", // რათა ახალი ციტატა წამოიღოს ყოველ ჯერზე
+  })
+    .then(function (quoteResponse) {
+      if (!quoteResponse.ok || quoteResponse.status === 404) {
+        throw new Error("ციტატა ვერ მოიძებნა");
+      }
+      return quoteResponse.json();
+    })
+    .then(function (responseData) {
+      const advice = responseData.slip.advice;
+      const id = responseData.slip.id;
+      adviceDemo.innerHTML = `"${advice}"`;
+      adviceNum.innerHTML = id;
+    })
+    .catch(function (error) {
+      adviceDemo.innerHTML = "დაფიქსირდა შეცდომა.";
+      document.getElementById("data").textContent =
+        error.message || "Server Error";
+    });
 });
 
-fetch("link", {
-  method: "GET",
-})
-  .then(function (quoteResponse) {})
-  .catch(function (error) {});
